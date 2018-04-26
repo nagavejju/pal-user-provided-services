59fa5bac1ec0f4086ef19e69235a92722210481c
Hunter Gillane <hgillane@pivotal.io>
2018-04-25 21:00:37 -0500

    add initializer

---
+++ src/main/java/io/pivotal/pal/paluserprovidedservices/PalUserProvidedServicesApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.pal.paluserprovidedservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalUserProvidedServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalUserProvidedServicesApplication.class, args);
+	}
+}
