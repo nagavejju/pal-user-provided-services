7f9c61fad6649d5b8da26e2097108bafeac3ff82
Georg Apitz <georg@usebar.org>
2018-04-26 08:02:48 -0500

    update text

--- src/main/java/io/pivotal/pal/paluserprovidedservices/HomeController.java
+++ src/main/java/io/pivotal/pal/paluserprovidedservices/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello world";
+        return "Hello PALs from Wells Fargo in Des Moines";
     }
 
 }
